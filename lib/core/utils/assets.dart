1185fe3b50bdae30f45e3e5009b8ea3e52f52b99
Nady Alwazah <nadysy.7@gmail.com>
2026-06-29 20:23:32 +0300

    feat(home): add AppBar actions with bag icon and notification badge

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const iconMenuSvg = "assets/icons/menu.svg";
   static const iconHighlightSvg = "assets/icons/Highlight.svg";
+  static const iconBagSvg = "assets/icons/bag.svg";
 }
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_task08_api_home_screen_beg/core/utils/app_colors.dart';
 import 'package:flutter_task08_api_home_screen_beg/core/utils/assets.dart';
 import 'package:flutter_task08_api_home_screen_beg/core/utils/styles.dart';
 
@@ -19,13 +21,36 @@ class HomeView extends StatelessWidget {
           children: [
             Text("Explore", style: Styles.textStyle32Bold),
             Positioned(
-              left: -17,
-              top: -6,
+              left: -17.w,
+              top: -6.h,
               child: SvgPicture.asset(AssetsData.iconHighlightSvg),
             ),
           ],
         ),
         centerTitle: true,
+        actions: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  backgroundColor: AppColors.backgroundColorItem,
+                  radius: 22.r,
+                  child: SvgPicture.asset(
+                    AssetsData.iconBagSvg,
+                    width: 24.r,
+                    height: 24.r,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 10.h,
+                right: 10.w,
+                child: CircleAvatar(backgroundColor: Colors.red, radius: 4.r),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
