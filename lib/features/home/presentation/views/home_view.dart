335e2618a3072cdc398fd029cf69feb72a32a97c
Nady Alwazah <nadysy.7@gmail.com>
2026-06-29 20:41:56 +0300

    chore(home): create initial HomeViewBody widget

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task08_api_home_screen_beg/features/home/presentation/views/widgets/home_app_bar.dart';
+import 'package:flutter_task08_api_home_screen_beg/features/home/presentation/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(appBar: HomeAppBar());
+    return const Scaffold(appBar: HomeAppBar(), body: HomeViewBody());
   }
 }
