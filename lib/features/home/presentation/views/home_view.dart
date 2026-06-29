d58b83190efd44dd760dd5a3db5fa1d0f70fd13e
Nady Alwazah <nadysy.7@gmail.com>
2026-06-29 20:37:41 +0300

    refactor(home): extract AppBar into separate HomeAppBar widget

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,61 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:flutter_task08_api_home_screen_beg/core/utils/app_colors.dart';
-import 'package:flutter_task08_api_home_screen_beg/core/utils/assets.dart';
-import 'package:flutter_task08_api_home_screen_beg/core/utils/styles.dart';
+import 'package:flutter_task08_api_home_screen_beg/features/home/presentation/views/widgets/home_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset(
-            AssetsData.iconMenuSvg,
-            width: 25.71.w,
-            height: 18.h,
-          ),
-        ),
-        title: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Text("Explore", style: Styles.textStyle32Bold),
-            Positioned(
-              left: -17.w,
-              top: -6.h,
-              child: SvgPicture.asset(AssetsData.iconHighlightSvg),
-            ),
-          ],
-        ),
-        centerTitle: true,
-        actions: [
-          Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  backgroundColor: AppColors.backgroundColorItem,
-                  radius: 22.r,
-                  child: SvgPicture.asset(
-                    AssetsData.iconBagSvg,
-                    width: 24.r,
-                    height: 24.r,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 10.h,
-                right: 10.w,
-                child: CircleAvatar(backgroundColor: Colors.red, radius: 4.r),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+    return const Scaffold(appBar: HomeAppBar());
   }
 }
